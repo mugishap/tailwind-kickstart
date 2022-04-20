fe343afd3eebb78ff9ac52f334b04715e0ca3e1a
precieux33 <precieuxmugisha@gmail.com>
2022-04-20 10:34:21 +0200

    react

--- server/apiget.js
+++ server/apiget.js
@@ -1,17 +1,14 @@
 import fetch from "node-fetch";
-const options = {
-    method: 'POST',
-    url: 'https://joke3.p.rapidapi.com/v1/joke',
-    headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Host': 'joke3.p.rapidapi.com',
-        'X-RapidAPI-Key': '4d3efa6d60mshe9647b4fc7ea6dbp1bf6dajsn7de302358e22'
-    }
-};
-const getData = async e => {
-    return await fetch('https://joke3.p.rapidapi.com/v1/joke', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+const cities = async () => {
+    await fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10', {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+            'X-RapidAPI-Key': '4d3efa6d60mshe9647b4fc7ea6dbp1bf6dajsn7de302358e22'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data.data))
 }
-console.log(getData())
+
+console.log(cities());
